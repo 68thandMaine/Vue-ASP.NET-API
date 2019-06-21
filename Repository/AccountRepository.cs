46e0406f1a0ecf8e9d27b159243344d6614c6bc8
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-06-21 15:38:46 -0700

    add: owner controller actions support full CRUD

--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Contracts;
 using Entities;
 using Entities.Models;
@@ -10,6 +12,10 @@ namespace Repository
         public AccountRepository(RepositoryContext repositoryContext)
         : base(repositoryContext)
         {
+        }
+        public IEnumerable<Account> AccountsByOwner(Guid ownerId)
+        {
+            return FindByCondition(a => a.OwnerId.Equals(ownerId)).ToList();
         }
     }
 }
--- Repository/OwnerRepository.cs
+++ Repository/OwnerRepository.cs
@@ -1,11 +1,11 @@
-﻿
-using System;
-using Contracts;
-using Entities;
-using Entities.Models;
-using Entities.ExtendedModels;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Contracts;
+using Entities;
+using Entities.ExtendedModels;
+using Entities.Extensions;
+using Entities.Models;
 
 namespace Repository
 {
@@ -19,14 +19,14 @@ namespace Repository
         public IEnumerable<Owner> GetAllOwners()
         {
             return FindAll()
-            .OrderBy(ow => ow.Name)
-            .ToList();
+                .OrderBy(ow => ow.Name)
+                .ToList();
         }
 
         public Owner GetOwnerById(Guid ownerId)
         {
             return FindByCondition(owner => owner.Id.Equals(ownerId))
-                 .DefaultIfEmpty(new Owner())
+                .DefaultIfEmpty(new Owner())
                 .FirstOrDefault();
         }
 
@@ -34,8 +34,26 @@ namespace Repository
         {
             return new OwnerExtended(GetOwnerById(ownerId))
             {
-                Accounts = RepositoryContext.Accounts.Where(a => a.OwnerId == ownerId)
+                Accounts = RepositoryContext.Accounts
+                    .Where(a => a.OwnerId == ownerId)
             };
+        }
+
+        public void CreateOwner(Owner owner)
+        {
+            owner.Id = Guid.NewGuid();
+            Create(owner);
+        }
+
+        public void UpdateOwner(Owner dbOwner, Owner owner)
+        {
+            dbOwner.Map(owner);
+            Update(dbOwner);
+        }
+
+        public void DeleteOwner(Owner owner)
+        {
+            Delete(owner);
         }
     }
 }
